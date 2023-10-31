00df1f5d28f9f0a48f5cf6224ca6037e54cc97fd
Joan Arc <joanedna11@gmail.com>
2023-10-31 22:40:05 +0700

    Tubes1 App

---
+++ app/src/main/java/com/example/tubes1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tubes1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.tubes1.databinding.ActivityMainBinding
+
+class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
