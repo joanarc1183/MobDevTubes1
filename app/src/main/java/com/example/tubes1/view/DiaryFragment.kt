eb59b3454ada952cc9ed17f297262dd735889fc3
Joan Arc <joanedna11@gmail.com>
2023-11-13 08:27:16 +0700

    draft photo
    unfinished

--- app/src/main/java/com/example/tubes1/view/DiaryFragment.kt
+++ app/src/main/java/com/example/tubes1/view/DiaryFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.tubes1.R
 import com.example.tubes1.databinding.FragmentDiaryBinding
 
 class DiaryFragment: Fragment() {
@@ -21,5 +22,14 @@ class DiaryFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // get data
+
+        val draftView = DraftFragment()
+        binding.edit.setOnClickListener(){
+            childFragmentManager.beginTransaction().replace(R.id.fragmentContainer, draftView)
+                .commit()
+        }
+
     }
 }
