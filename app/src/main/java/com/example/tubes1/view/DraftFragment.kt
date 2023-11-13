c90c80e8d6217142bd5e18a6d7b6c9ec6ad4b833
NirvanaSanky <6182101051@student.unpar.ac.id>
2023-11-14 00:55:39 +0700

    Nirvana

--- app/src/main/java/com/example/tubes1/view/DraftFragment.kt
+++ app/src/main/java/com/example/tubes1/view/DraftFragment.kt
@@ -1,4 +1,4 @@
-//package com.example.tubes1.view
+package com.example.tubes1.view//package com.example.tubes1.view
 //
 //import android.net.Uri
 //import android.os.Build
---
+++ app/src/main/java/com/example/tubes1/view/SettingFragment.kt
@@ -0,0 +1,33 @@
+package com.example.tubes1.viewcl
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.example.tubes1.R
+
+class SettingFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        return view
+
+        lateinit var switch: Switch
+        switch=view.findViewById(R.id.switcher)
+        switch.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+                false -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+    }
+
