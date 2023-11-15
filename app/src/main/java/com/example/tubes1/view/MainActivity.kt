29e4e83bd7e64c3945b98b6bf8f4074b57d28eb1
Joan Arc <joanedna11@gmail.com>
2023-11-15 23:22:32 +0700

    hehe coba darkmode blom nyala

--- app/src/main/java/com/example/tubes1/view/MainActivity.kt
+++ app/src/main/java/com/example/tubes1/view/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.tubes1.view
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.tubes1.R
@@ -35,14 +38,33 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(binding.fragmentContainer.id, imageFragment)
             .commit()
+
+        val switch: Switch = findViewById(R.id.switcher)
+        var currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            val newNightMode = if (isChecked) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+            if (currentNightMode != newNightMode) {
+                // Update the current night mode
+                currentNightMode = newNightMode
+
+                // Apply the new night mode
+                AppCompatDelegate.setDefaultNightMode(newNightMode)
+                delegate.applyDayNight()
+                setTheme(if (isChecked) R.style.AppTheme_Light else R.style.AppTheme_Dark)
+
+            }
+        }
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle hamburger icon click
         val id = item.itemId
         if (id == android.R.id.home) {
             val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
 
-            // Check if the drawer is open, if yes, close it; if not, open it
             if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
                 drawerLayout.closeDrawer(GravityCompat.START)
             } else {
